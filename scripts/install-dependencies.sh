86cf6a422b5cd40010d816b8803806b3dec16ebd
sweetClyde <jmvestal@gmail.com>
2020-03-07 18:20:04 -0500

    next step

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -3,7 +3,5 @@
 set -ev
 
 wget https://download2.interactivebrokers.com/installers/ibgateway/latest-standalone/ibgateway-latest-standalone-linux-x64.sh
-
-ls
 sudo chmod ugo+x ibgateway-latest-standalone-linux-x64.sh
-./ibgateway-latest-standalone-linux-x64.sh
+yes | ./ibgateway-latest-standalone-linux-x64.sh
