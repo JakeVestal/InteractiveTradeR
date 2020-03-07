6dcac68321907895d89cf6004dded8675facd90b
sweetClyde <jmvestal@gmail.com>
2020-03-07 15:17:55 -0500

    baby steps

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -4,6 +4,6 @@ set -ev
 
 wget https://download2.interactivebrokers.com/installers/ibgateway/latest-standalone/ibgateway-latest-standalone-linux-x64.sh
 
-echo yolo boiii
+pwd
 
-echo pwd
+ls ~/Downloads
