ecedc45155c65b8bb53dbfd796e1bdd00db04c04
sweetClyde <jmvestal@gmail.com>
2020-03-08 09:43:14 -0400

    remove ibg install script after installation

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -5,3 +5,4 @@ set -ev
 sudo wget -O scripts/ibgateway-latest-standalone-linux-x64.sh https://download2.interactivebrokers.com/installers/ibgateway/latest-standalone/ibgateway-latest-standalone-linux-x64.sh
 sudo chmod ugo+x scripts/ibgateway-latest-standalone-linux-x64.sh
 yes n | sudo ./scripts/ibgateway-latest-standalone-linux-x64.sh
+sudo rm scripts/ibgateway-latest-standalone-linux-x64.sh
